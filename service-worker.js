3406a6a07787d5bbab4888d78f6e4285d987eaf1
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-03-09 18:43:20 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "./precache-manifest.da4b4e47e5c7e9692d672d26e3036632.js"
 );
 
 self.addEventListener('message', (event) => {
